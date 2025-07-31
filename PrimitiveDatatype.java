4cbbe6e16f795afb2fe1b63e36079264d8950153
MercySylver <mercyben405@gmail.com>
2025-07-31 15:18:21 +0100

    primitive Datatype

---
+++ PrimitiveDatatype.java
@@ -0,0 +1,30 @@
+public class PrimitiveDatatype{
+	public static void main(String[] args){
+		//byte,short,int,long,float,double,char,boolean
+		byte age = 31;
+		System.out.println("Age = "+age);
+		
+		short quantity = 20000;
+		System.out.println("The Quantity Ordered is "+quantity);
+		
+		int nigeriaPopullation = 2000000000;
+		System.out.println("Nigeria population is "+ nigeriaPopullation);
+		
+		long worldspopulation = 46646464646L;
+		System.out.println("The Worlds populatis is "+worldspopulation);
+		
+		float weight = 76.456F;
+		System.out.println("My weight is "+weight);
+		
+		double accountBalance = 6430000.46;
+		System.out.println("My Account balance is $" +accountBalance);
+		
+		char symbol = '?';
+		System.out.println("What is your name"+symbol);
+		
+		boolean isJavaFun = true;
+		System.out.println("Is java fun " + isJavaFun);
+		
+		
+	}
+}
